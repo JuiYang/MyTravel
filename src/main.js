70b45480881fe28a1928ec6e1d04770bbe145637
JuiYang <894139169@qq.com>
2019-05-30 10:09:00 +0800

    初始化项目

---
+++ src/main.js
@@ -0,0 +1,21 @@
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import Vue from 'vue'
+import App from './App'
+import router from './router'
+import './assets/styles/reset.css'
+// 一像素边框问题
+import './assets/styles/border.css'
+// 移动端300ms点击事件延迟问题
+import fastClick from 'fastclick'
+
+Vue.config.productionTip = false
+fastClick.attach(document.body)
+
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  router,
+  components: { App },
+  template: '<App/>'
+})
---
+++ src/router/index.js
@@ -0,0 +1,19 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import HomeSite from '@/pages/home/HomeSite.vue'
+
+Vue.use(Router)
+
+export default new Router({
+  routes: [
+    {
+      path: '/',
+      name: 'HomeSite',
+      component: HomeSite
+    }, {
+      path: '/homeSite',
+      name: 'HomeSite',
+      component: HomeSite
+    }
+  ]
+})
