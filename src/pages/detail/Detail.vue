502554eb56d5679158b5c951e1659aa268d40c91
JuiYang <894139169@qq.com>
2019-06-14 15:54:15 +0800

    finish detail list

--- src/pages/detail/Detail.vue
+++ src/pages/detail/Detail.vue
@@ -3,6 +3,7 @@
     <detail-banner></detail-banner>
     <detail-header></detail-header>
     <div class="content">
+      <detail-list :list="list"></detail-list>
     </div>
   </div>
 </template>
@@ -10,11 +11,46 @@
 <script>
 import DetailBanner from './components/Banner.vue'
 import DetailHeader from './components/DetailHeader.vue'
+import DetailList from './components/List.vue'
 export default {
   name: 'Detail',
   components: {
     DetailBanner: DetailBanner,
-    DetailHeader: DetailHeader
+    DetailHeader: DetailHeader,
+    DetailList: DetailList
+  },
+  data () {
+    return {
+      list: [{
+        title: '成人票',
+        children: [{
+          title: '大连圣亚海洋馆'
+        }, {
+          title: '海洋动物世界滑稽表演'
+        }]
+      }, {
+        title: '学生票',
+        children: [{
+          title: '大连圣亚海洋馆'
+        }, {
+          title: '海洋动物世界滑稽表演'
+        }]
+      }, {
+        title: '大学生票',
+        children: [{
+          title: '大连圣亚海洋馆'
+        }, {
+          title: '海洋动物世界滑稽表演'
+        }]
+      }, {
+        title: '长者票',
+        children: [{
+          title: '大连圣亚海洋馆'
+        }, {
+          title: '海洋动物世界滑稽表演'
+        }]
+      }]
+    }
   }
 }
 </script>
---
+++ src/pages/detail/components/List.vue
@@ -0,0 +1,53 @@
+<template>
+  <div>
+    <div class="item" v-for="(item, key) of list" :key="key">
+      <div class="item-title border-bottom">
+        <span class="item-title-icon"></span>
+        {{item.title}}
+      </div>
+      <div v-if="item.children" class="item-children">
+        <detail-list :list="item.children"></detail-list>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'DetailList',
+  // props: {
+  //   swiperList: Array
+  // },
+  data () {
+    return {
+      showAbs: true
+    }
+  },
+  props: {
+    list: Array
+  },
+  methods: {
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+  @import '../../../assets/styles/variables.styl'
+  .item-children
+    padding-left: .2rem
+  .item-title
+    line-height: .8rem
+    font-size: .32rem
+    padding: 0 .2rem
+    text-align: left
+    .item-title-icon
+      position: relative
+      left: .06rem
+      top: .06rem
+      display: inline-block;
+      width: .36rem;
+      height: .36rem;
+      background: url(https://s.qunarzz.com/piao/image/touch/sight/detail.png) 0 -.45rem no-repeat;
+      margin-right: .1rem;
+      background-size: .4rem 3rem;
+</style>
